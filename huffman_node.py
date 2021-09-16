a8b5273963c67573f4bc7842427a82cc045f32e3
Akihiro Tanaka <akihiro.fin@icloud.com>
2021-09-17 01:12:05 +0300

    Nodes of Huffman tree

---
+++ huffman_node.py
@@ -0,0 +1,18 @@
+class HuffmanNode:
+    def __init__(self, character, frequency, left=None, right=None):
+        self.__character = character
+        self.__frequency = frequency
+        self.__left = left
+        self.__right = right
+
+    def get_character():
+        return self.__character
+    
+    def get_frequency():
+        return self.__frequency
+
+    def get_left_node():
+        return self.__left
+    
+    def get_right_node():
+        return self.__right
