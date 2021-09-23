4f57235c5cadbb42ca1f94686beb35fb254c1790
Akihiro Tanaka <akihiro.fin@icloud.com>
2021-09-24 00:12:26 +0300

    tests of the class HuffmanNode finished

--- src/tests/huffman_node_test.py
+++ src/tests/huffman_node_test.py
@@ -7,3 +7,14 @@ class TestHuffmanNode(unittest.TestCase):
     
     def test_get_character(self):
         self.assertEqual(self.node.get_character, "a")
+
+    def test_get_frequency(self):
+        self.assertEqual(self.node.get_frequency, 10)
+
+    def test_get_left_node(self):
+        node = HuffmanNode("b", 20, self.node)
+        self.assertEqual(node.get_left_node, self.node)
+
+    def test_get_right_node(self):
+        node = HuffmanNode("c", 30, None, self.node)
+        self.assertEqual(node.get_right_node, self.node)
