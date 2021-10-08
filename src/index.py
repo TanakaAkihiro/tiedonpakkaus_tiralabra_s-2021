a1eae2de3bb2b9669a9e40c8106c1e7feed91ad9
Akihiro Tanaka <akihiro.fin@icloud.com>
2021-10-08 20:08:44 +0300

    Print file sizes and time that required to compress/decompress

--- src/index.py
+++ src/index.py
@@ -1,7 +1,20 @@
 from config import FILE_PATH
 from huffman_coding import HuffmanCoding
+import time
 
 huffman = HuffmanCoding(FILE_PATH)
 
-huffman.compress()
-huffman.decompress()
+start = time.time()
+compressed = huffman.compress()
+end = time.time()
+
+print("Huffmanin koodauksen pakkaaminen: " + str((end - start) * 1000) + "ms")
+
+start = time.time()
+decompressed = huffman.decompress()
+end = time.time()
+
+print("Huffmanin koodauksen purkaminen: " + str((end - start) * 1000) + "ms")
+print()
+print("Tiedoston koko: " + str(decompressed/1000) + "kt")
+print("Tiedoston koko pakattuna Huffmanin koodauksella: " + str(compressed/1000) + "kt")
